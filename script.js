3675353b4a62796bd1fb9f9f095e84254f2ebf8c
Gerson Ramos <gerson.ramos@mag.gob.sv>
2024-05-17 23:46:03 -0600

    mend

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function login() {
 // CheckBox Function
 function goFurther(){
   if (document.getElementById("chkAgree").checked == true) {
-    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #FA4B37, #1E90FF);';
+    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #007991, #78ffd6);';
   }
   else{
     document.getElementById('btnSubmit').style = 'background: lightgray;';
